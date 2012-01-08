27130dd117979a31a3f8b68f0a4b0767b9e0fcc3
Dmitry S. Melnikov <dmitryme@dmitryme-thinkpad.(none)>
2012-01-08 22:14:48 +0400

    nitgogen moved to deps

--- build.sh
+++ build.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-mkdir -p rel/ebin
-
-cd metadata
-erl -make
-cp ebin/* ../rel/ebin
-cp src/metadata.app ../rel/ebin
-
-cd ..
-
-# build nitrogen framwwork
-
+mkdir -p rel/ebin/metadata
 mkdir -p rel/ebin/nitrogen_core
 mkdir -p rel/ebin/nprocreg
 mkdir -p rel/ebin/simple_bridge
+mkdir -p rel/ebin/site
 
-cd web_ui/nitrogen_core
+cd deps/nitrogen_core
 erl -pa ebin -make
-cp ebin/* ../../rel/ebin/nitrogen_core
+cp ebin/*.beam ../../rel/ebin/nitrogen_core
 
 cd ../nprocreg
 erl -pa ebin -make
-cp ebin/* ../../rel/ebin/nprocreg
+cp ebin/*.beam ../../rel/ebin/nprocreg
 cp src/nprocreg.app ../../rel/ebin
 
 cd ../simple_bridge
 erl -pa ebin -make
-cp ebin/* ../../rel/ebin/simple_bridge
+cp ebin/*.beam ../../rel/ebin/simple_bridge
 
-cd ../../
+cd ../../metadata
+
+erl -make
+cp ebin/*.beam ../rel/ebin/metadata
+cp src/metadata.app ../rel/ebin
+cp -r src/web_ui/templates ../rel/ebin/site
+cp -r src/web_ui/static ../rel/ebin/site
+
+cd ../..
