cf9abd1dac3a9d78cc7baba949d6cf3638841488
Dmitry S. Melnikov <dmitryme@dmitryme-netbook.(none)>
2012-01-09 13:56:49 +0400

    mtesrl lib

--- cpp/3rd_party/mtesrl/include/mtesrl/public.h
+++ cpp/3rd_party/mtesrl/include/mtesrl/public.h
@@ -10,10 +10,50 @@
 
 #include <windows.h>
 
+#include <string>
+
 #include <boost/cstdint.hpp>
 
-extern "C"
+namespace
 {
+
+int const MTE_OK             =  0
+int const MTE_CONFIG         = -1
+int const MTE_SRVUNAVAIL     = -2
+int const MTE_LOGERROR       = -3
+int const MTE_INVALIDCONNECT = -4
+int const MTE_NOTCONNECTED   = -5
+int const MTE_WRITE          = -6
+int const MTE_READ           = -7
+int const MTE_TSMR           = -8
+int const MTE_NOMEMORY       = -9
+int const MTE_ZLIB           = -10
+int const MTE_PKTINPROGRESS  = -11
+int const MTE_PKTNOTSTARTED  = -12
+int const MTE_LOGON          = -13
+int const MTE_FATALERROR     = MTE_LOGON // not used because of cabalistic signification
+int const MTE_INVALIDHANDLE  = -14
+int const MTE_DSROFF         = -15
+int const MTE_ERRUNKNOWN     = -16
+int const MTE_BADPTR         = -17
+int const MTE_WRONGPARAM     = MTE_BADPTR
+int const MTE_TRANSREJECTED  = -18
+int const MTE_REJECTION      = MTE_TRANSREJECTED
+int const MTE_TOOSLOWCONNECT = -19
+int const MTE_TEUNAVAIL      = MTE_TOOSLOWCONNECT
+int const MTE_CRYPTO_ERROR   = -20
+
+//Don't forget update this define
+#define _MTELastError       21
+
+int const MteTableUpdatable     = 1;
+int const MteTableClearOnUpdate = 2;
+
+int const ZlibCompressed = 0x1;
+int const FlagEncrypted  = 0x2;
+int const FlagSigningOn  = 0x4;
+
+} // namespace
 
 struct MTEMsg
 {
@@ -54,18 +94,6 @@ struct MTETables
    MTETable tables[1];
 };
 
-namespace
-{
-
-int const MteTableUpdatable     = 1;
-int const MteTableClearOnUpdate = 2;
-
-int const ZlibCompressed = 0x1;
-int const FlagEncrypted  = 0x2;
-int const FlagSigningOn  = 0x4;
-
-} // namespace
-
 struct ConnectionStats
 {
    boost::int32_t size;
@@ -76,52 +104,21 @@ struct ConnectionStats
    DWORD RecvBytes;
 };
 
-boost::int32_t WINAPI MTEOpenTable(boost::int32_t clientIdx, char* tableName, char* params, boost::int32_t completeFlag, MTEMsg** msg);
-boost::int32_t WINAPI MTEAddTable(boost::int32_t clientIdx, boost::int32_t htable, boost::int32_t ref);
-boost::int32_t WINAPI MTEExecTrans(boost::int32_t clientIdx, char* tranName, char* params, char* resultMsg);
-boost::int32_t WINAPI MTEStructure(boost::int32_t idx, MTEMsg** msg);
-boost::int32_t WINAPI MTEConnect(char* params, char* errMsg);
-boost::int32_t WINAPI MTERefresh(boost::int32_t clientIdx, MTEMsg** msg);
-boost::int32_t WINAPI MTECloseTable(boost::int32_t clientIdx, boost::int32_t htable);
-boost::int32_t WINAPI MTEDisconnect(boost::int32_t clientIdx);
-char* WINAPI MTEErrorMsg(boost::int32_t errorCode);
-boost::int32_t WINAPI MTEFreeBuffer(boost::int32_t clientIdx);
-boost::int32_t WINAPI MTEGetShapshot(boost::int32_t clientIdx, char** snapshot, int* len);
-boost::int32_t WINAPI MTESetShapshot(boost::int32_t clientIdx, char* snapshot, int len, char* error);
-boost::int32_t WINAPI MTEConnectionStats(boost::int32_t clientIdx, ConnectionStats* stats);
 
-
-} // extern "C"
-
-#define MTE_OK               0
-#define MTE_CONFIG          -1
-#define MTE_SRVUNAVAIL      -2
-#define MTE_LOGERROR        -3
-#define MTE_INVALIDCONNECT  -4
-#define MTE_NOTCONNECTED    -5
-#define MTE_WRITE           -6
-#define MTE_READ            -7
-#define MTE_TSMR            -8
-#define MTE_NOMEMORY        -9
-#define MTE_ZLIB           -10
-#define MTE_PKTINPROGRESS  -11
-#define MTE_PKTNOTSTARTED  -12
-#define MTE_LOGON          -13
-#define MTE_FATALERROR     MTE_LOGON // not used because of cabalistic signification
-#define MTE_INVALIDHANDLE  -14
-#define MTE_DSROFF         -15
-#define MTE_ERRUNKNOWN     -16
-#define MTE_BADPTR         -17
-#define MTE_WRONGPARAM     MTE_BADPTR
-#define MTE_TRANSREJECTED  -18
-#define MTE_REJECTION      MTE_TRANSREJECTED
-#define MTE_TOOSLOWCONNECT -19
-#define MTE_TEUNAVAIL      MTE_TOOSLOWCONNECT
-#define MTE_CRYPTO_ERROR   -20
-
-
-//Don't forget update this define
-#define _MTELastError       21
+void MTELoadLibrary(std::string const& fileName);
+boost::int32_t MTEOpenTable(boost::int32_t clientIdx, char* tableName, char* params, boost::int32_t completeFlag, MTEMsg** msg);
+boost::int32_t MTEAddTable(boost::int32_t clientIdx, boost::int32_t htable, boost::int32_t ref);
+boost::int32_t MTEExecTrans(boost::int32_t clientIdx, char* tranName, char* params, char* resultMsg);
+boost::int32_t MTEStructure(boost::int32_t clientIdx, MTEMsg** msg);
+boost::int32_t MTEConnect(char* params, char* errMsg);
+boost::int32_t MTERefresh(boost::int32_t clientIdx, MTEMsg** msg);
+boost::int32_t MTECloseTable(boost::int32_t clientIdx, boost::int32_t htable);
+boost::int32_t MTEDisconnect(boost::int32_t clientIdx);
+char* MTEErrorMsg(boost::int32_t errorCode);
+boost::int32_t MTEFreeBuffer(boost::int32_t clientIdx);
+boost::int32_t MTEGetShapshot(boost::int32_t clientIdx, char** snapshot, int* len);
+boost::int32_t MTESetShapshot(boost::int32_t clientIdx, char* snapshot, int len, char* error);
+boost::int32_t MTEConnectionStats(boost::int32_t clientIdx, ConnectionStats* stats);
 
 #pragma pack(pop)
 
---
+++ cpp/3rd_party/mtesrl/src/mtesrl.cpp
@@ -0,0 +1,144 @@
+/// @file   mtesrl.cpp
+/// @author Dmitry S. Melnikov, dmitryme@cqg.com
+/// @date   Created on: 01/09/2012 01:31:57 PM
+
+#include "../include/mtesrl/public.h"
+
+#include <sstream>
+
+typedef boost::int32_t (CALLBACK* MTEOpenTablePtr)(boost::int32_t, char*, char*, boost::int32_t, MTEMsg**);
+typedef boost::int32_t (CALLBACK* MTEAddTablePtr)(boost::int32_t, boost::int32_t, boost::int32_t);
+typedef boost::int32_t (CALLBACK* MTEExecTransPtr)(boost::int32_t, char*, char*, char*);
+typedef boost::int32_t (CALLBACK* MTEStructurePtr)(boost::int32_t, MTEMsg**);
+typedef boost::int32_t (CALLBACK* MTEConnectPtr)(char*, char*);
+typedef boost::int32_t (CALLBACK* MTERefreshPtr)(boost::int32_t, MTEMsg**);
+typedef boost::int32_t (CALLBACK* MTECloseTablePtr)(boost::int32_t, boost::int32_t);
+typedef boost::int32_t (CALLBACK* MTEDisconnectPtr)(boost::int32_t);
+typedef char* (CALLBACK* MTEErrorMsgPtr)(boost::int32_t);
+typedef boost::int32_t (CALLBACK* MTEFreeBufferPtr)(boost::int32_t);
+typedef boost::int32_t (CALLBACK* MTEGetShapshotPtr)(boost::int32_t, char**, int*);
+typedef boost::int32_t (CALLBACK* MTESetShapshotPtr)(boost::int32_t, char*, int, char*);
+typedef boost::int32_t (CALLBACK* MTEConnectionStatsPtr)(boost::int32_t, ConnectionStats*);
+
+HMODULE g_library = NULL;
+
+MTEOpenTablePtr         g_MTEOpenTable = NULL;
+MTEAddTablePtr          g_MTEAddTable = NULL;
+MTEExecTransPtr         g_MTEExecTrans = NULL;
+MTEStructurePtr         g_MTEStructure = NULL;
+MTEConnectPtr           g_MTEConnect = NULL;
+MTERefreshPtr           g_MTERefresh = NULL;
+MTECloseTablePtr        g_MTECloseTable = NULL;
+MTEDisconnectPtr        g_MTEDisconnect = NULL;
+MTEErrorMsgPtr          g_MTEErrorMsg = NULL;
+MTEFreeBufferPtr        g_MTEFreeBuffer = NULL;
+MTEGetShapshotPtr       g_MTEGetShapshot = NULL;
+MTESetShapshotPtr       g_MTESetShapshot = NULL;
+MTEConnectionStatsPtr   g_MTEConnectionStats = NULL;
+
+//---------------------------------------------------------------------------------------------------------------------//
+void MTELoadLibrary(std::string const& fileName)
+{
+   if (g_library)
+   {
+      return;
+   }
+   g_library = LoadLibrary(fileName.c_str());
+   if (!g_library)
+   {
+      std::ostringstream ost;
+      ost << "Failed to load " << fileName  << " library. Error = " << GetLastError();
+      throw std::runtime_error(ost.str());
+   }
+   g_MTEOpenTable =  (MTEOpenTablePtr)GetProcAddress(g_library, "MTEOpenTable");
+   g_MTEAddTable =   (MTEAddTablePtr)GetProcAddress(g_library, "MTEAddTable");
+   g_MTEExecTrans =  (MTEExecTransPtr)GetProcAddress(g_library, "MTEExecTrans");
+   g_MTEStructure =  (MTEStructurePtr)GetProcAddress(g_library, "MTEStructure");
+   g_MTEConnect =    (MTEConnectPtr)GetProcAddress(g_library, "MTEConnect");
+   g_MTERefresh =    (MTERefreshPtr)GetProcAddress(g_library, "MTERefresh");
+   g_MTECloseTable = (MTECloseTablePtr)GetProcAddress(g_library, "MTECloseTable");
+   g_MTEDisconnect = (MTEDisconnectPtr)GetProcAddress(g_library, "MTEDisconnect");
+   g_MTEErrorMsg =   (MTEErrorMsgPtr)GetProcAddress(g_library, "MTEErrorMsg");
+   g_MTEFreeBuffer =  (MTEFreeBufferPtr)GetProcAddress(g_library, "MTEFreeBuffer");
+   g_MTEGetShapshot = (MTEGetShapshotPtr)GetProcAddress(g_library, "MTEGetShapshot");
+   g_MTESetShapshot = (MTESetShapshotPtr)GetProcAddress(g_library, "MTESetShapshot");
+   g_MTEConnectionStats = (MTEConnectionStatsPtr)GetProcAddress(g_library, "MTEConnectionStats");
+}
+
+//---------------------------------------------------------------------------------------------------------------------//
+boost::int32_t MTEOpenTable(boost::int32_t clientIdx, char* tableName, char* params, boost::int32_t completeFlag, MTEMsg** msg)
+{
+   return g_MTEOpenTable(clientIdx, tableName, params, completeFlag, msg);
+}
+
+//---------------------------------------------------------------------------------------------------------------------//
+boost::int32_t MTEAddTable(boost::int32_t clientIdx, boost::int32_t htable, boost::int32_t ref)
+{
+   return g_MTEAddTable(clientIdx, htable, ref);
+}
+
+//---------------------------------------------------------------------------------------------------------------------//
+boost::int32_t MTEExecTrans(boost::int32_t clientIdx, char* tranName, char* params, char* resultMsg)
+{
+   return g_MTEExecTrans(clientIdx, tranName, params, resultMsg);
+}
+
+//---------------------------------------------------------------------------------------------------------------------//
+boost::int32_t MTEStructure(boost::int32_t clientIdx, MTEMsg** msg)
+{
+   return g_MTEStructure(clientIdx, msg);
+}
+
+//---------------------------------------------------------------------------------------------------------------------//
+boost::int32_t MTEConnect(char* params, char* errMsg)
+{
+   return g_MTEConnect(params, errMsg);
+}
+
+//---------------------------------------------------------------------------------------------------------------------//
+boost::int32_t MTERefresh(boost::int32_t clientIdx, MTEMsg** msg)
+{
+   return g_MTERefresh(clientIdx, msg);
+}
+
+//---------------------------------------------------------------------------------------------------------------------//
+boost::int32_t MTECloseTable(boost::int32_t clientIdx, boost::int32_t htable)
+{
+   return g_MTECloseTable(clientIdx, htable);
+}
+
+//---------------------------------------------------------------------------------------------------------------------//
+boost::int32_t MTEDisconnect(boost::int32_t clientIdx)
+{
+   return g_MTEDisconnect(clientIdx);
+}
+
+//---------------------------------------------------------------------------------------------------------------------//
+char* MTEErrorMsg(boost::int32_t errorCode)
+{
+   return g_MTEErrorMsg(errorCode);
+}
+
+//---------------------------------------------------------------------------------------------------------------------//
+boost::int32_t MTEFreeBuffer(boost::int32_t clientIdx)
+{
+   return g_MTEFreeBuffer(clientIdx);
+}
+
+//---------------------------------------------------------------------------------------------------------------------//
+boost::int32_t MTEGetShapshot(boost::int32_t clientIdx, char** snapshot, int* len)
+{
+   return g_MTEGetShapshot(clientIdx, snapshot, len);
+}
+
+//---------------------------------------------------------------------------------------------------------------------//
+boost::int32_t MTESetShapshot(boost::int32_t clientIdx, char* snapshot, int len, char* error)
+{
+   return g_MTESetShapshot(clientIdx, snapshot, len, error);
+}
+
+//---------------------------------------------------------------------------------------------------------------------//
+boost::int32_t MTEConnectionStats(boost::int32_t clientIdx, ConnectionStats* stats)
+{
+   return g_MTEConnectionStats(clientIdx, stats);
+}
