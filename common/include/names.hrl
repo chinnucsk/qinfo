e280be89c37f50621412bb26630b95dbdae8885c
Dmitry Melnikov <dmitryme@gmail.com>
2012-01-11 09:23:20 +0400

    Update common/include/names.hrl

--- common/include/names.hrl
+++ common/include/names.hrl
@@ -2,6 +2,8 @@
 -define(group_rts_bba,           'RTS.bba').
 -define(group_rts_trades,        'RTS.trades').
 
+-define(group_micex_instruments,   'MICEX.instruments').
+
 -define(qinfo_metadata,     'qinfo.metadata').
 -define(qinfo_plaza2,       'qinfo.plaza2').
 -define(qinfo_micex_mtesrl, 'qinfo.micex_mtesrl').
