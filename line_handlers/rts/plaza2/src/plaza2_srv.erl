b9f032601a945effa6efc2f0382c0d5e71e81b6c
Dmitry Melnikov <dmitryme@gmail.com>
2012-01-11 09:39:07 +0400

    Update line_handlers/rts/plaza2/src/plaza2_srv.erl

--- line_handlers/rts/plaza2/src/plaza2_srv.erl
+++ line_handlers/rts/plaza2/src/plaza2_srv.erl
@@ -4,13 +4,17 @@
 
 -export([start/0, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--define(plaza2_port_dll, "plaza2_port").
+-define(plaza2_driver_dll, "plaza2_driver").
 
 -include("names.hrl").
 -include("metadata.hrl").
 
--define(def_settings, [{ini_file, "P2ClientGate.ini"}, {host, "192.168.1.99"}, {port, 4001}, {app_name, "qinfo"},
-      {passwd, "123"}, {log_level, info}]).
+-define(def_settings,
+   [
+      {service, [{log_level, info}]},
+      {plaza2,  [{ini_file, "P2ClientGate.ini"}, {host, "192.168.1.99"}, {port, 4001}, {app_name, "qinfo"},
+            {passwd, "123"}, {log_level, info}]}
+   ]).
 -define(def_schedule,
    [
       {mon, {10, 0, 0}, {23, 00, 00}},
@@ -37,7 +41,7 @@ init(_Args) ->
       ?qinfo_plaza2, ?def_settings, ?def_schedule),
    Settings = extract_settings(SList),
    error_logger:info_msg("~p settings: ~p.~n", [?qinfo_plaza2, Settings]),
-   ok = erl_ddll:load_driver(".", ?plaza2_port_dll),
+   ok = erl_ddll:load_driver(".", ?plaza2_driver_dll),
    DrvPort = open(Settings#settings.ini_file, Settings#settings.host, Settings#settings.port, Settings#settings.app_name,
       Settings#settings.passwd, Settings#settings.log_level,
       [
@@ -46,12 +50,12 @@ init(_Args) ->
    ),
    {ok, #state{drv_port = DrvPort, settings = Settings}}.
 
-handle_call(Request, _From, State) ->
-   io:format("~p~n", [Request]),
+handle_call(Msg, _From, State) ->
+   error_logger:error_msg("Unexpected message: ~p~n", [Msg]),
    {reply, undef, State}.
 
 handle_cast(Msg, State) ->
-   error_logger:warning_msg("Unexpected message: ~p~n", [Msg]),
+   error_logger:error_msg("Unexpected message: ~p~n", [Msg]),
    {noreply, State}.
 
 handle_info({_Port, {data, Msg}}, State) ->
@@ -71,12 +75,12 @@ code_change(_OldVsn, State, _Extra) ->
 %% ========= private ===========
 
 open(IniFile, Host, Port, AppName, Password, LogLevel, Streams) ->
-   DrvPort = erlang:open_port({spawn, ?plaza2_port_dll}, [binary]),
-   erlang:port_command(DrvPort, term_to_binary({connect, IniFile, Host, Port, AppName, Password, LogLevel, Streams})),
+   DrvPort = erlang:open_port({spawn, ?plaza2_driver_dll}, [binary]),
+   true = erlang:port_command(DrvPort, term_to_binary({connect, IniFile, Host, Port, AppName, Password, LogLevel, Streams})),
    DrvPort.
 
 close(DrvPort) ->
-   erlang:port_command(DrvPort, term_to_binary({disconnect})),
+   true = erlang:port_command(DrvPort, term_to_binary({disconnect})),
    port_close(DrvPort).
 
 processInfo(
@@ -107,6 +111,16 @@ processInfo(
       ref = SessId * 1000000000 + IsinId},
    pg:send(?group_rts_instruments, Instr);
 
+processInfo(#'FORTS_FUTINFO_REPL.fut_sess_contents'{}) ->
+   ok;
+processInfo({log_info, Str}) ->
+   error_logger:info_msg(Str);
+processInfo({log_debug, Str}) ->
+   error_logger:info_msg(Str);
+processInfo({log_error, Str}) ->
+   error_logger:error_msg(Str);
+processInfo({log_warn, Str}) ->
+   error_logger:warning_msg(Str);
 processInfo(Msg) ->
    ok.
 
@@ -120,12 +134,12 @@ format_datetime(DateTime) ->
    MSec = DateTime rem 1000,
    {{Year, Month, Day},{Hour, Min, Sec, MSec}}.
 
-extract_settings(SLst) ->
+extract_settings([{service, SList}, {plaza2, PList}]) ->
    #settings{
-      ini_file = element(2, lists:keyfind(ini_file, 1, SLst)),
-      host     = element(2, lists:keyfind(host, 1, SLst)),
-      port     = element(2, lists:keyfind(port, 1, SLst)),
-      app_name = element(2, lists:keyfind(app_name, 1, SLst)),
-      passwd   = element(2, lists:keyfind(passwd, 1, SLst)),
-      log_level = element(2, lists:keyfind(log_level, 1, SLst))
+      ini_file = element(2, lists:keyfind("ini_file", 1, PList)),
+      host     = element(2, lists:keyfind("host", 1, PList)),
+      port     = list_to_integer(element(2, lists:keyfind("port", 1, PList))),
+      app_name = element(2, lists:keyfind("app_name", 1, PList)),
+      passwd   = element(2, lists:keyfind("passwd", 1, PList)),
+      log_level = list_to_atom(element(2, lists:keyfind("log_level", 1, SList)))
    }.
