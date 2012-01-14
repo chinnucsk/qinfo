2a5425da6c19d4166ef84cfee5550ccbaa0979c2
unknown <user@home-5833cac43e.(none)>
2012-01-14 21:24:25 +0300

    rebar added

--- common/include/names.hrl
+++
@@ -1,9 +0,0 @@
--define(group_rts_instruments,   'rts.instruments').
--define(group_rts_bba,           'rts.bba').
--define(group_rts_trades,        'rts.trades').
-
--define(group_micex_instruments,   'micex.instruments').
-
--define(qinfo_metadata,     'qinfo.metadata').
--define(qinfo_rts_plaza2,   'qinfo.rts.plaza2').
--define(qinfo_micex_mtesrl, 'qinfo.micex.mtesrl').
