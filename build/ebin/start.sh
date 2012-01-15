2f7c03b1b44083ccf292369f92f43119fa4230b3
Dmitry S. Melnikov <dmitryme@dmitryme-thinkpad.(none)>
2012-01-15 23:32:40 +0400

    Linuc build

--- build/ebin/start.sh
+++ build/ebin/start.sh
@@ -2,4 +2,6 @@
 
 export ERL_LIBS=.
 
-erl -boot start_sasl -metadata inets_port 8000 -config start.config -eval "application:start(metadata)"
+erl -boot start_sasl -metadata inets_port 8000 -config start.config \
+   -eval "application:start(nprocreg)" \
+   -eval "application:start(metadata)"
