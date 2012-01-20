7e57db146ff6526efe890365ab68abf4a9968c59
Dmitry Melnikov <dmitryme@gmail.com>
2012-01-20 18:10:33 +0400

    Update cpp/plaza2_driver/plaza2_driver_dll.cpp

--- cpp/plaza2_driver/plaza2_driver_dll.cpp
+++ cpp/plaza2_driver/plaza2_driver_dll.cpp
@@ -18,10 +18,11 @@
 
 using namespace Plaza2;
 
-ei_cxx::Port g_port;
+struct PortData;
 
-void process_connect(ei_cxx::ITuple&);
-void process_disconnect();
+void process_init(PortData*, ei_cxx::ITuple&);
+void process_connect(PortData*, ei_cxx::ITuple&);
+void process_disconnect(PortData*);
 
 BOOL APIENTRY DllMain( HANDLE hModule,
                        DWORD  ul_reason_for_call,
@@ -41,32 +42,32 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 
 struct PortData
 {
-   ErlDrvPort port;
+   ei_cxx::Port port;
+   Connection* conn;
 };
 
 //------------------------------------------------------------------------------------------------------------------------//
 PLAZA2_DRIVER_DLL_API ErlDrvData start(ErlDrvPort port, char *buff)
 {
    PortData* d = (PortData*)driver_alloc(sizeof(PortData));
-    d->port = port;
-    if (!g_port)
-    {
-      g_port = port;
-    }
-    return (ErlDrvData)d;
+   d->port = port;
+   d->conn = new Connection(d->port);
+   return reinterpret_cast<ErlDrvData>(d);
 }
 
 //------------------------------------------------------------------------------------------------------------------------//
 PLAZA2_DRIVER_DLL_API void stop(ErlDrvData handle)
 {
-   Connection::instance()->disconnect();
+   PortData* pd = reinterpret_cast<PortData*>(handle);
+   pd->conn->disconnect();
+   delete pd->conn;
    driver_free((char*)handle);
-   g_port = NULL;
 }
 
 //------------------------------------------------------------------------------------------------------------------------//
 PLAZA2_DRIVER_DLL_API void received(ErlDrvData drv_data, ErlIOVec *ev)
 {
+   PortData* pd = reinterpret_cast<PortData*>(drv_data);
    try
    {
       using namespace ei_cxx;
@@ -79,13 +80,17 @@ PLAZA2_DRIVER_DLL_API void received(ErlDrvData drv_data, ErlIOVec *ev)
             bin >> tuple;
             Atom command_name;
             tuple >> command_name;
+            if (command_name.get() == "init")
+            {
+               process_init(pd, tuple);
+            }
             if (command_name.get() == "disconnect")
             {
-               process_disconnect();
+               process_disconnect(pd);
             }
             else if (command_name.get() == "connect")
             {
-               process_connect(tuple);
+               process_connect(pd, tuple);
             }
             else if (command_name.get() == "log_level")
             {
@@ -106,17 +111,25 @@ PLAZA2_DRIVER_DLL_API void received(ErlDrvData drv_data, ErlIOVec *ev)
    }
    catch(std::exception const& err)
    {
-      ERL_LOG_ERROR(g_port, err.what());
+      ERL_LOG_ERROR(pd->port, err.what());
    }
    catch(_com_error const& err)
    {
-      ERL_LOG_ERROR(g_port,
+      ERL_LOG_ERROR(pd->port,
          "received(): COM error. Error=" << err.Error() << ", Message=" << err.ErrorMessage());
    }
 }
 
+/// {init, iniFile}
+void process_init(PortData*, ei_cxx::ITuple& t)
+{
+   std::string iniFile;
+   t >> iniFile;
+   static Application app(iniFile);
+}
+
 // {connect, iniFile, "host", port, "app_name", "password", log_level, [{FUT_TRADE_REPL, iniFile, StreamType}]}.
-void process_connect(ei_cxx::ITuple& t)
+void process_connect(PortData* pd, ei_cxx::ITuple& t)
 {
    using namespace ei_cxx;
    std::string iniFile;
@@ -131,7 +144,7 @@ void process_connect(ei_cxx::ITuple& t)
    t >> host >> port >> appName >> password >> llevel;
    log_level = LogLevel::fromString(llevel.get());
 
-   Connection::instance()->connect(host, port, appName, password);
+   pd->conn->connect(host, port, appName, password);
 
    IList streams;
    t >> streams;
@@ -144,14 +157,14 @@ void process_connect(ei_cxx::ITuple& t)
       std::string iniFile;
       Atom streamType;
       stream >> streamName >> iniFile >> streamType;
-      Connection::instance()->addStream(streamName, iniFile, StreamType::fromString(streamType.get()));
+      pd->conn->addStream(streamName, iniFile, StreamType::fromString(streamType.get()));
    }
 }
 
 // {disconnect}
-void process_disconnect()
+void process_disconnect(PortData* pd)
 {
-   Connection::instance()->disconnect();
+   pd->conn->disconnect();
 }
 
 PLAZA2_DRIVER_DLL_API ErlDrvEntry plaza2_driver_entry =
