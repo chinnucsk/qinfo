604f8c4c14cd18cb094f9be9aa52fd6c436800c8
Dmitry Melnikov <dmitryme@gmail.com>
2012-01-24 18:38:35 +0400

    Update apps/common/src/common_utils.erl

--- apps/common/src/common_utils.erl
+++ apps/common/src/common_utils.erl
@@ -10,7 +10,7 @@ load_dll() ->
    erlang:load_nif(code:priv_dir(common) ++ "/common_utils", 0).
 
 cp1251_to_unicode(_) ->
-   exit(n)if_library_not_loaded).
+   exit(nif_library_not_loaded).
 
 list_to_atom(Lst) ->
    case catch(list_to_existing_atom(Lst)) of
