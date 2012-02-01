0c10e8d85c984f551b1d07bae29fcef539ff2296
Dmitry Melnikov <dmitryme@gmail.com>
2012-02-01 18:54:10 +0400

    Update apps/metadata/src/www/instruments.erl

--- apps/metadata/src/www/instruments.erl
+++ apps/metadata/src/www/instruments.erl
@@ -68,23 +68,24 @@ build_exchanges(Key, Exchanges) ->
 
 build_instr_header() ->
    #tablerow{ cells = [
-      #tableheader{ text = "Name",       style = "width: 110px;"},
-      #tableheader{ text = "Full name",  style = "width: 300px;"},
-      #tableheader{ text = "QinfoName",  style = "width: 100px;"},
-      #tableheader{ text = "ClassCode",  style = "width: 100px;"},
-      #tableheader{ text = "ExchName",   style = "width: 100px;"},
-      #tableheader{ text = "Type",       style = "width: 80px;"},
-      #tableheader{ text = "Lot size",   style = "width: 80px;"},
-      #tableheader{ text = "Expiration", style = "width: 100px;"},
-      #tableheader{ text = "Alias",      style = "width: 100px;"},
-      #tableheader{ text = "Enabled",    align = center}
+      #tableheader{ text = "Name",        style = "width: 110px;"},
+      #tableheader{ text = "Full name",   style = "width: 300px;"},
+      #tableheader{ text = "QinfoName",   style = "width: 100px;"},
+      #tableheader{ text = "ClassCode",   style = "width: 100px;"},
+      #tableheader{ text = "ExchName",    style = "width: 100px;"},
+      #tableheader{ text = "Type",        style = "width: 80px;"},
+      #tableheader{ text = "Lot size",    style = "width: 80px;"},
+      #tableheader{ text = "Expiration",  style = "width: 100px;"},
+      #tableheader{ text = "LastUpdated", style = "width: 150px;"},
+      #tableheader{ text = "Enabled",     style = "width: 80px;"},
+      #tableheader{ text = "Alias",       style = "width: 160px;"}
    ], style = "background-color: #999797;"}.
 
 build_instr(Alpha, Exchanges, TypeList) ->
    AlphaList = lists:flatten(
       [ [ [#literal{ text = [X]}, #literal{ text = " "}] || X <- lists:seq($0, $9)],
         [ [#literal{ text = [X]}, #literal{ text = " "}] || X <- lists:seq($A, $Z)]]),
-   EnabledOnly = get_enabled_only(),
+   EnabledOnly = is_checked(checkbox_enabled),
    {NewAlphaList, Instrs} = build_instr_impl(Alpha, AlphaList, TypeList, EnabledOnly, Exchanges, mnesia:dirty_first(m_commodity),
    ?white),
    InstrHeader = build_instr_header(),
@@ -120,7 +121,8 @@ build_by_commodity(#m_commodity{key = Key = {_, Type, Exch}, class_code = ClassC
       (_,_) -> false end, Instruments),
    Size = length(SortedInstruments),
    lists:foldl(
-      fun(#m_instrument{key = {ExchName, _, _}, expiration = Expiration, full_name = FullName, lot_size = LSize}, []) ->
+      fun(#m_instrument{key = {ExchName, _, _}, expiration = Expiration, updated = Updated, full_name = FullName, lot_size = LSize}, []) ->
+            CheckId = wf:temp_id(),
             [#tablerow{ cells = [
                #tablecell{ text = ExchName },
                #tablecell{ text = unicode:characters_to_binary(FullName) },
@@ -130,10 +132,11 @@ build_by_commodity(#m_commodity{key = Key = {_, Type, Exch}, class_code = ClassC
                #tablecell{ text = Type },
                #tablecell{ text = LSize },
                #tablecell{ text = expiration_to_list(Expiration)},
-               #tablecell{ body = [ #textbox{ text = alias_to_list(Alias), style = "width: 100px;"}], rowspan = if (Size == 1) -> 0; true -> Size end},
-               #tablecell{ body = [ #checkbox{ checked = Enabled}], rowspan = if (Size == 1) -> 0; true -> Size end}
+               #tablecell{ text = date_to_list(Updated)},
+               #tablecell{ body = [ #checkbox{ id = CheckId, checked = Enabled, postback = {checkbox_enabled, CheckId, Key}}], rowspan = if (Size == 1) -> 0; true -> Size end},
+               #tablecell{ body = [ #inplace_textbox{ text = alias_to_list(Alias), style = "width: 160px;"}], rowspan = if (Size == 1) -> 0; true -> Size end}
             ], style = "background-color: " ++ BackColor}];
-        (#m_instrument{key = {ExchName, _, _}, expiration = Expiration, full_name = FullName, lot_size = LSize}, Acc) ->
+        (#m_instrument{key = {ExchName, _, _}, expiration = Expiration, updated = Updated, full_name = FullName, lot_size = LSize}, Acc) ->
              Acc ++ [#tablerow{ cells = [
                #tablecell{ text = ExchName },
                #tablecell{ text = unicode:characters_to_binary(FullName) },
@@ -142,13 +145,13 @@ build_by_commodity(#m_commodity{key = Key = {_, Type, Exch}, class_code = ClassC
                #tablecell{ text = Exch },
                #tablecell{ text = Type },
                #tablecell{ text = LSize },
-               #tablecell{ text = expiration_to_list(Expiration)}
+               #tablecell{ text = expiration_to_list(Expiration)},
+               #tablecell{ text = date_to_list(Updated)}
             ], style = "background-color: " ++ BackColor}]
       end, [], SortedInstruments).
 
-
 alias_to_list(undef) ->
-   "";
+   "-";
 alias_to_list(Alias) ->
    Alias.
 
@@ -156,6 +159,9 @@ expiration_to_list(undef) ->
    "-";
 expiration_to_list({{Year, Month, Day},_}) ->
    lists:flatten(io_lib:format("~2.10.0B.~2.10.0B.~.10B", [Day, Month, Year])).
+
+date_to_list({{Y,M,D},{H,MM,S}}) ->
+   lists:flatten(io_lib:format("~2.10.0B.~2.10.0B.~4.10B ~2.10.0B.~2.10.0B.~2.10B", [D, M, Y, H, MM, S])).
 
 get_type_list() ->
    TypeList = wf:qs(checkbox_type),
@@ -166,13 +172,18 @@ event({alpha, A}) ->
    {AlphaFilter, Instruments} = build_instr(A, wf:qs(checkbox_exchange), get_type_list()),
    wf:replace(alpha_filter, AlphaFilter),
    wf:replace(instruments, Instruments);
+
 event(filter_changed) ->
    {AlphaFilter, Instruments} = build_instr(wf:state(alpha), wf:qs(checkbox_exchange), get_type_list()),
    wf:replace(alpha_filter, AlphaFilter),
-   wf:replace(instruments, Instruments).
+   wf:replace(instruments, Instruments);
 
-get_enabled_only() ->
-   case wf:q(checkbox_enabled) of
+event({checkbox_enabled, CheckId, CommodityKey}) ->
+   [Commodity] = mnesia:dirty_read(m_commodity, CommodityKey),
+   ok = mnesia:dirty_write(Commodity#m_commodity{ enabled = is_checked(CheckId) }).
+
+is_checked(Checkbox) ->
+   case wf:q(Checkbox) of
       "on" ->
          true;
       _ ->
