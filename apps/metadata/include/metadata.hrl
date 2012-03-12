39e8a9b8b96c0cd553560330d7e596b473f8d206
Dmitry Melnikov <dmitryme@gmail.com>
2012-03-12 17:45:17 +0400

    Update apps/metadata/include/metadata.hrl

--- apps/metadata/include/metadata.hrl
+++ apps/metadata/include/metadata.hrl
@@ -5,11 +5,17 @@
 -type hour() :: pos_integer().
 -type min()  :: pos_integer().
 -type time_interval() :: {{hour(), min()}, {hour(), min()}}.
--type commodity_key() :: {commodity(), type(), exchange()}.
--type instrument_key() :: {exch_name(), type(), exchange()}.
 -type day_of_week() :: 'mon' | 'tue' | 'wed' | 'thu' | 'fri' | 'sat' | 'sun'.
 -type sched_of_day() :: {day_of_week(), 'enabled' | 'disabled', string()}. % E.g. {mon, enabled, "09:30-15:30"}
 -type sched_of_day_fmt() :: {pos_integer(), 'enabled' | 'disabled', [time_interval()]}.
+
+-record(instrument_key, {exch_symbol  :: exch_symbol(),
+                         type         :: type,
+                         exchange     :: exchange}).
+
+-record(commodity_key, {commodity     :: commodity(),
+                        type          :: type(),
+                        exchange      :: exchange()}).
 
 -record(new_instrument, {name         :: string(),
                          exch_name    :: string(),
@@ -33,26 +39,26 @@
                   settings :: list(),
                   schedule :: sched_of_day()}).
 
--record(m_commodity, {key             :: commodity_key(),
-                      class_code      :: string(),
-                      enabled = false :: boolean(),
-                      alias = undef   :: 'undef' | string()}).
+-record(commodity, {key             :: #commodity_key{},
+                    class_code      :: string(),
+                    enabled = false :: boolean(),
+                    alias = undef   :: 'undef' | string()}).
 
--record(m_instrument, {key                :: instrument_key(),
-                       commodity          :: commodity_key(),
-                       full_name          :: string(),
-                       expiration = undef :: 'undef' | calendar:datetime(),
-                       limit_up           :: float(),
-                       limit_down         :: float(),
-                       lot_size           :: pos_integer(),
-                       updated            :: calendar:datetime(),
-                       ref                :: pos_integer()}).
+-record(instrument, {key                :: #instrument_key{},
+                     commodity          :: #commodity_key{},
+                     full_name          :: string(),
+                     expiration = undef :: 'undef' | calendar:datetime(),
+                     limit_up           :: float(),
+                     limit_down         :: float(),
+                     lot_size           :: pos_integer(),
+                     updated            :: calendar:datetime(),
+                     ref                :: pos_integer()}).
 
--record(m_service, {service            :: tuple(),
-                    description        :: string(),
-                    settings = []      :: list(),
-                    schedule = []      :: [sched_of_day()],
-                    fmt_schedule = []  :: [sched_of_day_fmt()]}).
+-record(service, {service            :: tuple(),
+                  description        :: string(),
+                  settings = []      :: list(),
+                  schedule = []      :: [sched_of_day()],
+                  fmt_schedule = []  :: [sched_of_day_fmt()]}).
 
--record(m_exchange, {name            :: string(),
-                    description = "" :: string()}).
+-record(exchange, {name            :: string(),
+                   description = "" :: string()}).
