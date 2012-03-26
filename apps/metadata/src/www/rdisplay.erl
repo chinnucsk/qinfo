922a4d9a77e2abe661fa597adb01bcd7f014bf2a
Dmitry S. Melnikov <dmitryme@dmitryme-thinkpad.(none)>
2012-03-26 22:40:01 +0400

    frames

--- apps/metadata/src/www/rdisplay.erl
+++ apps/metadata/src/www/rdisplay.erl
@@ -1,1 +1,14 @@
 -module(rdisplay).
+
+-include_lib("nitrogen_core/include/wf.hrl").
+
+-export([main/0, layout/0, title/0]).
+
+title() ->
+   "qinfo:logs".
+
+main() ->
+   #template{file="./www/rdisplay.html"}.
+
+layout() ->
+   "".
--- apps/metadata/src/www/reports.erl
+++ apps/metadata/src/www/reports.erl
@@ -1,1 +1,30 @@
 -module(reports).
+
+-include_lib("nitrogen_core/include/wf.hrl").
+
+-export([main/0, layout/0, title/0]).
+
+title() ->
+   "qinfo:logs".
+
+main() ->
+   #template{file="./www/reports.html"}.
+
+layout() ->
+   TopPanel = #panel{
+      body =
+      [
+         #link{ class=a, text = "main", url = "index"},
+         #literal{ text = " | "},
+         #link{ class=a, text = "settings", url = "settings"},
+         #literal{ text = " | " },
+         #link{ class=a, text = "scheduler", url = "scheduler"},
+         #literal{ text = " | " },
+         #link{ class=a, text = "instruments", url = "instruments"},
+         %#literal{ text = " | "},
+         %#link{ class=a, text = "statistics", url = "statistics"},
+         #literal{ text = " | "},
+         #literal{ text = "logs"}
+      ]
+   },
+   TopPanel.
