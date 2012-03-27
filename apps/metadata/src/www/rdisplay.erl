ea4ef8b2bf14c8e2104dab2f4c3fe144691b4fb4
Dmitry Melnikov <dmitryme@gmail.com>
2012-03-27 16:20:44 +0400

    Update apps/metadata/src/www/rdisplay.erl

--- apps/metadata/src/www/rdisplay.erl
+++ apps/metadata/src/www/rdisplay.erl
@@ -1,14 +1,28 @@
--module(rdisplay).
+-module(rdetails).
 
 -include_lib("nitrogen_core/include/wf.hrl").
 
 -export([main/0, layout/0, title/0]).
 
-title() ->
-   "qinfo:logs".
+title() -> "".
 
 main() ->
    #template{file="./www/rdisplay.html"}.
 
 layout() ->
-   "".
+   {ok, Pid} = wf:comet(fun() -> process_msg() end, rdetails),
+   wf:session(rdetails, Pid),
+   #panel{id = field}.
+
+
+process_msg() ->
+   receive
+      'INIT' ->
+         wf:insert_bottom(field, #literal{text = "First one!"}),
+         wf:flush(),
+         process_msg();
+      {details, No} ->
+         log_viewer:show(No),
+         wf:flush(),
+         process_msg()
+   end.
