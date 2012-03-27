a8378c0f2087a77c03d4bd871751be2de8b631a1
Dmitry Melnikov <dmitryme@gmail.com>
2012-03-27 16:19:48 +0400

    Update apps/metadata/src/www/reports.erl

--- apps/metadata/src/www/reports.erl
+++ apps/metadata/src/www/reports.erl
@@ -2,7 +2,7 @@
 
 -include_lib("nitrogen_core/include/wf.hrl").
 
--export([main/0, layout/0, title/0]).
+-export([main/0, layout/0, title/0, event/1]).
 
 title() ->
    "qinfo:logs".
@@ -22,9 +22,37 @@ layout() ->
          #literal{ text = " | " },
          #link{ class=a, text = "instruments", url = "instruments"},
          %#literal{ text = " | "},
-         %#link{ class=a, text = "statistics", url = "statistics"},
+         %#link{ class=a, text = "statistics", url = "statistics"}
          #literal{ text = " | "},
          #literal{ text = "logs"}
       ]
    },
-   TopPanel.
+   Records = build_records(),
+   [
+      TopPanel,
+      Records
+   ].
+
+build_records() ->
+   Records = log_viewer:list(),
+   Header = #tablerow{ cells = [
+      #tableheader{text = "Date",         style = "width: 150px;"},
+      #tableheader{text = "NN",           style = "width: 70px;" },
+      #tableheader{text = "Type",         style = "width: 70px;" },
+      #tableheader{text = "Process",      style = "width: 150px;"}
+   ], style = "background-color: #999797;"},
+   #table{rows = [ Header | build_records(Records)]}.
+build_records([]) ->
+   [];
+build_records([{No, Type,  ShortDescr, Date}|T]) ->
+   [
+      #tablerow{ cells = [
+         #tablecell{text = Date},
+         #tablecell{body = [#link{text = No, postback = {details, No}}]},
+         #tablecell{text = Type},
+         #tablecell{text = ShortDescr}]} | build_records(T)
+   ].
+
+event(M = {details, _No}) ->
+   Pid = wf:session(rdetails),
+   Pid ! M.
