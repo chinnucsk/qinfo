180c1869351d0ce3ecc3e711784755e7d0ad2d23
Dmitry Melnikov <dmitryme@gmail.com>
2012-03-29 17:41:35 +0400

    Update cpp/include.cmake

--- cpp/include.cmake
+++ cpp/include.cmake
@@ -1,6 +1,7 @@
 cmake_minimum_required(VERSION 2.6)
 
 set(BOOST_HOME    C:/dev/qinfo/cpp/3rd_party/boost_1_48_0/)
+set(BOOST_INCLUDE ${BOOST_HOME})
 set(BOOST_LIB     ${BOOST_HOME}stage/lib)
 
 set(ERL_HOME      C:/Program\ Files/erl5.9/)
@@ -9,6 +10,6 @@ set(ERL_LIB       ${ERL_HOME}usr/lib)
 set(EI_INCLUDE    ${ERL_HOME}lib/erl_interface-3.7.6/include)
 set(EI_LIB        ${ERL_HOME}lib/erl_interface-3.7.6/lib)
 
-include_directories(${BOOST_HOME} ${ERL_INCLUDE} ${EI_INCLUDE})
+include_directories(${BOOST_INCLUDE} ${ERL_INCLUDE} ${EI_INCLUDE})
 
 link_directories(${BOOST_LIB} ${EI_LIB} ${ERL_LIB})
