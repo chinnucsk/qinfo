df4e8dce3c03257e61f4dcb918776251c56dbecd
Dmitry Melnikov <dmitryme@gmail.com>
2012-05-07 16:49:01 +0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ call "%VS100COMNTOOLS%\vsvars32.bat"
 
 set ROOT=%CD%
 set LogFile=%ROOT%\build.log
-set BuildType=Debug
+set BuildType=Release
 
 mkdir build\cpp
 pushd build\cpp
@@ -12,7 +12,9 @@ popd
 
 escript.exe rebar compile
 
-xcopy apps\common\ebin\*.beam build\ebin\common\ebin\  /Y
+xcopy deps\log_viewer\ebin\*.beam build\ebin\log_viewer\ebin\ /Y
+xcopy deps\log_viewer\ebin\*.app build\ebin\ /Y
+xcopy apps\qinfo_common\ebin\*.beam build\ebin\qinfo_common\ebin\  /Y
 xcopy apps\micex_mtesrl\ebin\*.beam build\ebin\micex_mtesrl\ebin\  /Y
 xcopy apps\micex_mtesrl\ebin\*.app build\ebin\ /Y
 xcopy apps\rts_plaza2\ebin\*.beam build\ebin\rts_plaza2\ebin\  /Y
@@ -25,6 +27,6 @@ xcopy apps\nitrogen_core\ebin\*.beam build\ebin\nitrogen_core\ebin\ /Y
 xcopy apps\nprocreg\ebin\*.beam build\ebin\nprocreg\ebin\ /Y
 xcopy apps\nprocreg\ebin\*.app build\ebin\ /Y
 xcopy apps\simple_bridge\ebin\*.beam build\ebin\simple_bridge\ebin\ /Y
-xcopy build\cpp\Debug\micex_driver.dll build\ebin\micex_mtesrl\priv\ /Y
-xcopy build\cpp\Debug\plaza2_driver.dll build\ebin\rts_plaza2\priv\ /Y
-xcopy build\cpp\Debug\common_utils.dll build\ebin\common\priv\ /Y
+xcopy build\cpp\%BuildType%\micex_driver.dll build\ebin\micex_mtesrl\priv\ /Y
+xcopy build\cpp\%BuildType%\plaza2_driver.dll build\ebin\rts_plaza2\priv\ /Y
+xcopy build\cpp\%BuildType%\qinfo_common_utils.dll build\ebin\qinfo_common\priv\ /Y
